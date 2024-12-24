6dc627aef2700d6537ec0b6a0f559f9ecc49aca3
ieGo <zen@lan.uz>
2024-12-24 19:30:04 +0400

    LLM-API

---
+++ 02 - LLM API with Python, token streaming/generate-blogpost.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+const readline = require("readline");
+const OpenAI = require("openai");
+const fs = require('node:fs');
+const args = process.argv.slice(2);
+let transcript = args[1];
+let apiKey = args[0] || process.env.OPENAI_API_KEY;
+
+
+function promptForInput(question) {
+  return new Promise((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl.question(question, (answer) => {
+      rl.close();
+      resolve(answer);
+    });
+  });
+}
+async function main() {
+  if (!apiKey) {
+    apiKey = await promptForInput("Please enter your OpenAI API key: ");
+  }
+  if (!transcript) {
+    transcript = await promptForInput("Please provide transcript: (default: lesson-1-transcript.txt) ");
+
+    if(!transcript) {
+      transcript = 'lesson-1-transcript.txt';
+    }
+  }
+  const data = fs.readFileSync(transcript, 'utf8');
+  const openai = new OpenAI({
+    apiKey: apiKey
+  });
+  
+  try {
+    const completion = await openai.chat.completions.create({
+      model: "gpt-4o",
+      messages: [
+        { role: "system", content: 'You are an expert blog writer. When given transcript, create a well-structured blog post.' },
+        {
+          role: 'user',
+          content: `
+            Transcript: ${data}
+            
+            Please write a detailed blog post using Transcript.
+          `,
+        },
+      ],
+    });
+
+    console.log("Response:", completion.choices[0].message.content);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+main();
